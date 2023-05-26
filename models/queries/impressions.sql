fc7704f62fa18e4fcc75bbccdb3950ef060d90c5
agil.yolchuyev <agilyolchuyev@gmail.com>
2023-05-26 03:33:46 +0000

    field name change

--- models/queries/impressions.sql
+++ models/queries/impressions.sql
@@ -1,19 +1,19 @@
 with
-    cost_per_engage as (
-        select channel, sum(impressions) as `impressions`
+    impressions as (
+        select channel, sum(impressions) as impressions
         from {{ ref("src_promoted_tweets_twitter_all_data") }}
         group by channel
         union all
-        select channel, sum(impressions) as `impressions`
+        select channel, sum(impressions) as impressions
         from {{ ref("src_ads_creative_facebook_all_data") }}
         group by channel
         union all
-        select channel, 0 as `impressions`
+        select channel, 0 as impressions
         from {{ ref("src_ads_bing_all_data") }}
         group by channel
         union all
-        select channel, sum(impressions) as `impressions`
+        select channel, sum(impressions) as impressions
         from {{ ref("src_ads_tiktok_ads_all_data") }}
         group by channel
     )
-select * from cost_per_engage
+select * from impressions
