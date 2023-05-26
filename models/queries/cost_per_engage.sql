73a176e85fc3761f6212d92b9d751c34cfed88e1
agil.yolchuyev <agilyolchuyev@gmail.com>
2023-05-26 01:22:56 +0000

    update

--- models/queries/cost_per_engage.sql
+++ models/queries/cost_per_engage.sql
@@ -24,11 +24,4 @@ with
         group by channel
     )
 select *
-from
-    cost_per_engage
-
-    -- Select * from {{ ref("src_ads_creative_facebook_all_data") }}
-    -- select channel, round(sum(spend) / sum(comments + shares + views +
-    -- mobile_app_install + inline_link_clicks ), 2) as `cost per engage` from {{
-    -- ref("src_ads_creative_facebook_all_data") }} group by channel
-    
+from cost_per_engage
